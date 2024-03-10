1fe5a11ff676e93a037bc46cb6447c9f5ef7aa71
tanvirtbf <tanver018765@gmail.com>
2024-03-10 12:16:27 +0600

    Linking and Navigation

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,17 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const About = () => {
+  const router = useRouter()
+  const navigate = (routeName)=>{
+    router.push(routeName)
+  }
   return (
     <div>
       <h1>About Page</h1>
+      <button onClick={()=> navigate('/')}>Go to Home Page</button>
+      <button onClick={()=> navigate('/login')}>Go to Login Page</button>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
 "use client"
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
+import Link from 'next/link'
 
 export default function Home() {
-  const apple=(item)=>{
-    alert(item)
-  }
-  const InnerComp = ()=>{
-    return (
-      <h1>Hello Inner Component</h1>
-    )
+  const router = useRouter()
+  const navigate = (route)=>{
+    router.push(route)
   }
   return (
     <main className={styles.main}>
       <h1>Home Page</h1>
-      <button onClick={()=> apple('Fruit')}>Click Me</button>
-      <InnerComp />
+      <Link href={"/login"}>Go to Login Page</Link>
+      <Link href={"/about"}>Go to About Page</Link>
+      <button onClick={()=> navigate('login')}>Go to Login page</button>
+      <button onClick={()=> navigate('about')}>Go to About Page</button>
     </main>
   );
 }
