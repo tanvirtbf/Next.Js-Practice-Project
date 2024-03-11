27957272a13a94521c573aace83317ffd0025d6f
tanvirtbf <tanver018765@gmail.com>
2024-03-11 09:45:22 +0600

    404 page for globally and specific 404 page using segment routes concept [...name]

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>This is not Not found About Page</h1>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      404 sorry Not Found!
+    </div>
+  )
+}
+
+export default NotFound
