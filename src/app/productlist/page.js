ea564766c175ccd8ca73e8c2e09836eeb0f233c3
tanvirtbf <tanver018765@gmail.com>
2024-03-11 14:10:46 +0600

    call api in server component

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,23 +1,42 @@
-"use client";
-import { useEffect, useState } from "react";
-export default function Page() {
-  const [products,setProducts] = useState([])
-  const getData = async () => {
-    const data = await fetch("https://dummyjson.com/products");
-    const res = await data.json();
-    setProducts(res.products)
-    console.log(products)
-  };
-  useEffect(() => {
-    getData();
-  });
-// 01883259988
+async function productlist() {
+  let data = await fetch("https://dummyjson.com/products");
+  data = await data.json();
+  return data.products;
+}
+
+export default async function Page() {
+  const getData = await productlist();
+  console.log(getData);
   return (
     <div>
       <h1>Product List</h1>
-      {
-        products.map((item,i)=> <h2 key={i}>{item.title}</h2>)
-      }
+      {getData.map((item, i) => (
+        <h3 key={i}>{item.title}</h3>
+      ))}
     </div>
   );
 }
+
+// "use client";
+// import { useEffect, useState } from "react";
+// export default function Page() {
+//   const [products,setProducts] = useState([])
+//   const getData = async () => {
+//     const data = await fetch("https://dummyjson.com/products");
+//     const res = await data.json();
+//     setProducts(res.products)
+//     console.log(products)
+//   };
+//   useEffect(() => {
+//     getData();
+//   });
+// // 01883259988
+//   return (
+//     <div>
+//       <h1>Product List</h1>
+//       {
+//         products.map((item,i)=> <h2 key={i}>{item.title}</h2>)
+//       }
+//     </div>
+//   );
+// }
