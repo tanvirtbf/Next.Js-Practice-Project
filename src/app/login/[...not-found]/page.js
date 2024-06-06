401ef761644537557d6a7bdd808ac3b5162a2545
tanvirtbf <tanver018765@gmail.com>
2024-06-06 17:33:30 +0600

    usePathname with catch all route segments test

--- src/app/login/[...not-found]/page.js
+++ src/app/login/[...not-found]/page.js
@@ -1,6 +1,10 @@
+"use client"
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const NotFound = () => {
+  const pathName = usePathname()
+  console.log(pathName)
   return (
     <div>
       Not Found Vai!
