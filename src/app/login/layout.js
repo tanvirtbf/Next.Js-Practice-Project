053992c399e24b7f7aaad3688171940e449f25d9
tanvirtbf <tanver018765@gmail.com>
2024-06-06 14:19:00 +0600

    implement conditional layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,9 +1,14 @@
+"use client"
 import React from 'react'
-
+import { usePathname } from 'next/navigation'
 const RootLayout = ({children}) => {
+  const pathName = usePathname()
+  console.log(pathName)
   return (
     <div>
-      <h1>Common Layout for Login</h1>
+      {
+        pathName !== "/login/loginteacher" && <h1>Common Layout for Login</h1>
+      }
       {children}
     </div>
   )
