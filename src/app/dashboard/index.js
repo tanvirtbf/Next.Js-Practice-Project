04c5e73d00fdd363cf63adde961766b8d26b0662
tanvirtbf <tanver018765@gmail.com>
2024-06-07 23:57:40 +0600

    end this day

--- src/app/dashboard/index.js
+++ src/app/dashboard/index.js
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
-import "./styles.css"
+import "./styles.css";
 
 const Dashboard = () => {
   const router = useRouter();
--- src/app/studentlist/[...student]/page.js
+++ src/app/studentlist/[...student]/page.js
@@ -1,17 +1,22 @@
-"use client"
-import { useParams, usePathname } from 'next/navigation'
-import React from 'react'
+"use client";
+import { useParams, usePathname } from "next/navigation";
 
-const Student = ({params}) => {
-  const {student} = useParams()
-  const pathName = usePathname()
+const Student = ({ params }) => {
+  const { student } = useParams();
+  const pathName = usePathname();
   return (
     <div>
-      {
-        pathName === "/studentlist/sadia" ? <h1>Hi Sadia</h1> : pathName === "/studentlist/maymuna" ? <h1>Hello Maymuna</h1> : pathName === "/studentlist/humaira" ? <h1>Bye Humaira</h1> : <h1>{`This is ${student}`}</h1>
-      }
+      {pathName === "/studentlist/sadia" ? (
+        <h1>Hi Sadia</h1>
+      ) : pathName === "/studentlist/maymuna" ? (
+        <h1>Hello Maymuna</h1>
+      ) : pathName === "/studentlist/humaira" ? (
+        <h1>Bye Humaira</h1>
+      ) : (
+        <h1>{`This is ${student}`}</h1>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Student
+export default Student;
