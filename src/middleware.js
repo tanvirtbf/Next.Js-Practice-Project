25b23b3d1a9ed3a49ed5b44fc9a9cc678943ccb2
tanvirtbf <tanver018765@gmail.com>
2024-06-08 11:20:44 +0600

    working with csr ssg ssr

--- src/middleware.js
+++ src/middleware.js
@@ -2,3 +2,8 @@ import { NextResponse } from 'next/server'
 export function middleware(req){
   return NextResponse.redirect(new URL("/login",req.url))
 }
+
+export const config={
+  matcher:"/about/:path*"
+}
+
