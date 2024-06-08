572502a66ce774a13a7f02943ace5446716489d4
tanvirtbf <tanver018765@gmail.com>
2024-06-08 11:28:20 +0600

    let's fetch api in this project

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,11 @@
-"use client";
+"use client"
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-
+import { useEffect } from "react";
 export default function Home() {
-  const router = useRouter();
-
-  const routing = (route) => {
-    router.push(route);
-  };
-
   return (
     <main>
-      <h1>Basic Routing | Make New Page</h1>
-      <Link href={"/login"}>Login Page</Link>
-      <br />
-      <Link href={"/about"}>About Page</Link>
-      <br />
-      <br />
-      <button onClick={() => routing("/login")}>Login</button>
-      <br />
-      <br />
-      <button onClick={() => routing("/about")}>About</button>
-      <br />
-      <br />
-      <button onClick={() => router.push("/studentlist")}>Student List</button>
+      <h1>Hello World!</h1>
+      <Link href={"/productlist"}>Go to Product List</Link>
     </main>
   );
 }
--- src/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-export function middleware(req){
-  return NextResponse.redirect(new URL("/login",req.url))
-}
-
-export const config={
-  matcher:"/about/:path*"
-}
-
