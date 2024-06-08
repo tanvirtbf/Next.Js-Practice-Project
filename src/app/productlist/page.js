13e63c33ff23a6cb59c7f83c6f04e2573a2dcf1b
tanvirtbf <tanver018765@gmail.com>
2024-06-08 12:08:07 +0600

    bug fixed

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 const ProductList = () => {
+  const [product,setProduct] = useState([])
+  const getData = async () => {
+    let data = await fetch("https://dummyjson.com/products");
+    data = await data.json();
+    setProduct(data.products)
+  }
+  useEffect(()=>{
+    getData();
+  },[])
   return (
     <div>
       <h1>Product List</h1>
+      {
+        product.map((item,i)=> <h3 key={i}>{item.title}</h3>)
+      }
     </div>
   )
 }
