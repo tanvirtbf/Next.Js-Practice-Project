c290972f97fa0997018baf1738444d8da46aad34
tanvirtbf <tanver018765@gmail.com>
2024-06-08 14:20:29 +0600

    call api with client component testing

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,22 +1,25 @@
-import React from 'react'
+"use client"
 
-async function productlists(){
-  let data = await fetch("https://dummyjson.com/products");
-  data = await data.json();
-  return data.products;
-}
+import { useEffect, useState } from "react";
 
-const ProductList = async () => {
-  const data = await productlists();
+const ProductList = () => {
+  const [product,setProduct] = useState([]);
+  const getData = async () => {
+    let data = await fetch("https://dummyjson.com/products");
+    data = await data.json();
+    setProduct(data.products);
+  }
+  useEffect(()=> {
+    getData();
+  },[])
   return (
     <div>
-      <h1>Product List</h1>
+      <h1>Call api with Client Component</h1>
       {
-        data.map((item,i)=> <h3 key={i}>{item.title}</h3>)
+        product.map((item,i)=> <h3 key={i}>{item.title}</h3>)
       }
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
-
+export default ProductList;
