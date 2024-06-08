a10205d8b931809ca35d1af9811195c97182c352
tanvirtbf <tanver018765@gmail.com>
2024-06-08 17:55:44 +0600

    ends this day

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import Button from './Button';
+import Button from "./Button";
 
 const getData = async () => {
   let data = await fetch("https://dummyjson.com/products");
   data = await data.json();
   return data.products;
-}
+};
 
 const ProductList = async () => {
   const data = await getData();
   return (
     <div>
       <h1>Call Api with Server Component</h1>
-      {
-        data.map((item,i)=> {
-          return (
-            <>
-              <h3 key={i}>{item.title}</h3>
-              <Button price = {item.price} />
-            </>
-          )
-        })
-      }
+      {data.map((item, i) => {
+        return (
+          <>
+            <h3 key={i}>{item.title}</h3>
+            <Button price={item.price} />
+          </>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
 
-export function generateMetadata({params}){
-  return (
-    {
-      title: "hello",
-      description: "Hello World!"
-    }
-  )
+export function generateMetadata({ params }) {
+  return {
+    title: "hello",
+    description: "Hello World!",
+  };
 }
