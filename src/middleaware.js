760d68bc18036142f76becab1ec02c4122323f83
tanvirtbf <tanver018765@gmail.com>
2024-06-08 09:40:38 +0600

    start with middleware

--- src/middleaware.js
+++ src/middleaware.js
@@ -1,7 +1,1 @@
-import { NextResponse } from "next/server";
-export function middleware(req) {
-  return NextResponse.redirect(new URL("/login", req.url));
-}
-export const config = {
-  matcher: ["/about/:path*", "/studentlist/:path*"],
-};
+
