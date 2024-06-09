83273f4c0e1265335da24c43bb13847486ec306a
tanvirtbf <tanver018765@gmail.com>
2024-06-09 10:04:21 +0600

    start from scratch for practice

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
-      <Link href={"/productlist"}>Go to Product List</Link>
+      <h1>Hello World! my boy</h1>
     </main>
   );
 }
